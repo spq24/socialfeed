8390494bd7cc4a03c82d9bd5b167789b5e2c9379
spq24 <spq2461@gmail.com>
2014-10-19 21:28:20 -0400

    figured out twitter

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -3,14 +3,30 @@ class FeedsController < ApplicationController
 
 
 	def index
-		@twitter = Twitter::REST::Client.new do |config|
-		  config.consumer_key        = "RrbWaUSooiqroLB3FXKQ1s8Et"
-		  config.consumer_secret     = "j4QY89XrXaeGNiKj3q0r3PxZdRKbADEZHQzXfCpqn0ZGBTTtZk"
-		  config.access_token        = "277562395-W9mCZHviUhRILUN8cQQ9DiThIkd5uK1SbIMny9Xe"
-		  config.access_token_secret = "qZ3FUtxXnPCzebY0ZKYdlOVzd63tBicTUH0rsb49XwGRS"
+		client = Twitter::REST::Client.new do |config|
+		  config.consumer_key        = ENV["twitter_consumer_key"]
+		  config.consumer_secret     = ENV["twitter_consumer_secret"]
+		  config.access_token        = ENV["twitter_access_token"]
+		  config.access_token_secret = ENV["twitter_access_token_secret"]
 		end
+
+		
+
+		def collect_with_max_id(collection=[], max_id=nil, &block)
+		  response = yield(max_id)
+		  collection += response
+		  response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
+		end
+
+		def client.get_all_tweets(user)
+		  collect_with_max_id do |max_id|
+		    options = {:count => 200, :include_rts => true}
+		    options[:max_id] = max_id unless max_id.nil?
+		    user_timeline(user, options)
+		  end
+		end
+
+		@twitter = client.get_all_tweets(277562395)
 	end
-
-
 
 end
